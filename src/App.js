bf03bd5994ed5eb9dff47539e252092aa4fa4a91
Ahmad Dayeh <jnet230@gmail.com>
2024-10-15 23:03:12 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { GoogleOAuthProvider, GoogleLogin } from "@react-oauth/google";
+import axios from "axios";
 
-function App() {
+const App = () => {
+  const [backendResponse, setBackendResponse] = useState(null);
+
+  const handleLoginSuccess = (credentialResponse) => {
+    const token = credentialResponse.credential;
+    axios
+      .post("http://localhost:5000/auth/google-signin", { token })
+      .then((response) => {
+        setBackendResponse(response.data);
+      })
+      .catch((error) => {
+        console.error("Error sending token to backend:", error);
+      });
+  };
+
+  const handleLoginFailure = () => {
+    console.error("Google login failed");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <GoogleOAuthProvider clientId="662678382199-t3gh90mmsm7k4hdpn51sajtrvo0iv71j.apps.googleusercontent.com">
+      <div style={{ textAlign: "center", marginTop: "50px" }}>
+        <h1>React Google OAuth Demo</h1>
+        <GoogleLogin
+          onSuccess={handleLoginSuccess}
+          onError={handleLoginFailure}
+        />
+        {backendResponse && (
+          <div>
+            <h3>Backend Response:</h3>
+            <pre>{JSON.stringify(backendResponse, null, 2)}</pre>
+          </div>
+        )}
+      </div>
+    </GoogleOAuthProvider>
   );
-}
+};
 
 export default App;
